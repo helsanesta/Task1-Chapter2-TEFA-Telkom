2c2f9eea63c8e0e887782bb9dc9cc07424aa8c52
Helsa Nesta <70515589+helsanesta@users.noreply.github.com>
2023-05-08 08:59:55 +0700

    first commit: dbcon, usermodel, usercontroller

---
+++ controllers/userController.go
@@ -0,0 +1,38 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"task1-ch2-api-product/database"
+
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validate = validator.New()
+
+// HashPassword is used to encrypt the password before it is stored in the DB
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return string(bytes)
+}
+
+// VerifyPassword checks the input password while verifying it with the passward in the DB.
+func VerifyPassword(userPassword string, providedPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
+
+	if err != nil {
+		msg = fmt.Sprintf("The password you entered is incorrect")
+		check = false
+	}
+
+	return check, msg
+}
